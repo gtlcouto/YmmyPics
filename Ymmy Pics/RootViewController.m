c35c95620b7e4c7c63984d28db4df220535d14ea
Gustavo Couto <gtlcouto@Gustavos-MacBook-Pro.local>
2015-02-02 15:23:44 -0800

    first Commit

--- Ymmy Pics/RootViewController.m
+++ Ymmy Pics/RootViewController.m
@@ -6,22 +6,17 @@
 //  Copyright (c) 2015 Gustavo Couto. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "RootViewController.h"
 
-@interface ViewController ()
+@interface RootViewController ()
 
 @end
 
-@implementation ViewController
+@implementation RootViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
